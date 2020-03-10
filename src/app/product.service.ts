a4816d702cd894ce0c479541cbf18ace550f4c1b
silvana <garciaserrano.1@osu.edu>
2020-03-10 15:56:06 -0500

    task3

---
+++ src/app/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import {map} from 'rxjs/operators';
+
+@Injectable(
+)
+
+
+export class ProductService {
+
+  constructor(private _http: Http) { }
+
+  _albumUrl: string = '../assets/album.json';
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).pipe()
+  }
+
+}
