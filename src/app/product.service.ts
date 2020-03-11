0b44ce4538474276514c48d4366b4e13054e1264
silvana <garciaserrano.1@osu.edu>
2020-03-11 09:17:03 -0500

    task 4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import {map} from 'rxjs/operators';
 import 'rxjs/add/operator/map';
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable(
 )
@@ -13,7 +15,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).pipe(map((response) => response.json() ))
   }
 
